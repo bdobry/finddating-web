f6b057b5c424fc9556720a9ddfb702f957c00598
bdobry <bartoszdobry@gmail.com>
2016-10-31 21:19:35 +0100

    Make categories description appear on top

--- archive.php
+++ archive.php
@@ -8,7 +8,10 @@
 		
 			<div class="page-title">
 					
+				
 				<div class="section-inner">
+          
+          
 
 					<h4><?php if ( is_day() ) : ?>
 						<?php printf( __( 'Date: %s', 'lovecraft' ), '' . get_the_date( get_option('date_format') ) . '' ); ?>
@@ -44,7 +47,24 @@
 			<?php if ( have_posts() ) : ?>
 			
 				<?php rewind_posts(); ?>
-					
+				
+        <?php 
+          $cat_desc = category_description($category_id);
+          $stripped = wp_strip_all_tags($cat_desc, '<p>'); //replace <p> and <a> with whatever tags you want to keep after the strip
+        ?>
+        <div class="category" style="margin-bottom: 30px;
+    font-family: Open Sans, sans-serif;
+    padding: 20px 0 20px 40px;
+    border-left: 5px solid #ca2017;">
+				  <h1 class="widget-title" style="    border: none;
+				      font-size: 20px;
+				      color: #333;
+				      font-weight: 900;"><?php single_cat_title(); ?></h1>
+				  <p style="    line-height: 25px;
+    font-size: 15px;
+    max-width: 600px;"><?php echo $stripped; ?></p>
+				</div>
+				
 				<div class="posts" id="posts">
 					
 					<?php while ( have_posts() ) : the_post(); ?>
