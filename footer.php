f8e15d97beecebe4e96a281139bf99c18b5a8cf9
bdobry <bartoszdobry@gmail.com>
2016-10-31 22:22:59 +0100

    Switch footer content to the right side

--- footer.php
+++ footer.php
@@ -1,45 +1,53 @@
 <?php if ( is_active_sidebar('footer-one') || is_active_sidebar('footer-two') || is_active_sidebar('footer-three') ) : ?>
 
-	<div class="footer section big-padding bg-white">
-		
-		<div class="section-inner">
-			
-			<div class="widgets">
-				
-				<?php dynamic_sidebar('footer-one'); ?>
-				
-			</div>
-			
-			<div class="widgets">
-				
-				<?php dynamic_sidebar('footer-two'); ?>
-				
-			</div>
-			
-			<div class="widgets">
-				
-				<?php dynamic_sidebar('footer-three'); ?>
-				
-			</div>
-			
-			<div class="clear"></div>
-			
-		</div> <!-- /section-inner -->
-		
-	</div> <!-- /footer.section -->
+<div class="footer section big-padding bg-white">
+
+  <div class="section-inner">
+    <div class="content" style="margin-top:0;">
+
+      <div class="widgets">
+
+        <?php dynamic_sidebar('footer-one'); ?>
+
+      </div>
+
+      <div class="widgets">
+
+        <?php dynamic_sidebar('footer-two'); ?>
+
+      </div>
+
+      <div class="widgets">
+
+        <?php dynamic_sidebar('footer-three'); ?>
+
+      </div>
+
+      <div class="clear"></div>
+
+    </div>
+    <div class="clear"></div>
+    <!-- /section-inner -->
+
+  </div>
+  <!-- /footer.section -->
+</div>
 
 <?php endif; ?>
 
 <div class="credits section bg-dark">
-			
-	<div class="credits-inner section-inner">
-	
-	
-	</div> <!-- /section-inner -->
 
-</div> <!-- /credits.section -->
+  <div class="credits-inner section-inner">
+
+
+  </div>
+  <!-- /section-inner -->
+
+</div>
+<!-- /credits.section -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
